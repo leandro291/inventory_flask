01b3c9bfe2b166cee265f205df97a4bbab074462
leandro291 <lenadrito1@gmail.com>
2026-06-28 12:33:32 -0500

    Agregar modelo Role para gestionar roles en la base de datos

---
+++ app/models/role_model.py
@@ -0,0 +1,17 @@
+from db import db
+from sqlalchemy import Integer, String, Text, Boolean, func, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+
+class Role(db.Model):
+
+    __tablename__ = "roles"
+
+    id_role: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    status: Mapped[bool] = mapped_column(Boolean, default=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
+    
+    
