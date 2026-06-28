9b8106f9aff19ec3eb61e2c12d5b4a0ef940a67e
leandro291 <lenadrito1@gmail.com>
2026-06-28 12:27:07 -0500

    Agregar modelo Company para gestionar empresas en la base de datos

---
+++ app/models/company_model.py
@@ -0,0 +1,16 @@
+from db import db
+from datetime import datetime
+from sqlalchemy import Integer, String, Boolean, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+class Company(db.Model):
+
+    __tablename__ = "companies"
+
+    id_company: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True)
+    ruc: Mapped[str] = mapped_column(String(11), nullable=False)
+    address: Mapped[str] = mapped_column(String(255), nullable=False)
+    status: Mapped[bool] = mapped_column(Boolean, default=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
