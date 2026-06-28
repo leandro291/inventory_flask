baa3509a397892286634d440d730c07b0f9ce6ab
leandro291 <lenadrito1@gmail.com>
2026-06-28 13:44:19 -0500

    Agregar restricción de nulabilidad al campo 'name' en el modelo Company

--- app/models/company_model.py
+++ app/models/company_model.py
@@ -8,7 +8,7 @@ class Company(db.Model):
     __tablename__ = "companies"
 
     id_company: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String(255), unique=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     ruc: Mapped[str] = mapped_column(String(11), nullable=False)
     address: Mapped[str] = mapped_column(String(255), nullable=False)
     status: Mapped[bool] = mapped_column(Boolean, default=True)
