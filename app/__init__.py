bc0fd44565c364cadcb17949cb719221e61f78cc
leandro291 <lenadrito1@gmail.com>
2026-06-28 13:49:20 -0500

    Reorganizar importaciones de modelos en el archivo __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,3 +8,16 @@ app.config.from_object(Config)
 db.init_app(app)
 
 migrate = Migrate(app, db)
+
+from app.models import (
+    role_model,
+    user_model,
+    company_model,
+    product_model,
+    movement_model,
+    category_model,
+    supplier_model,
+    inventory_model,
+    repository_model,
+    type_movement_model
+)
