b8007dec2343b78f7be6c8a8722415324ab90d64
leandro291 <lenadrito1@gmail.com>
2026-06-28 12:54:01 -0500

    Agregar modelo TypeMovement para gestionar tipos de movimiento en la base de datos

---
+++ app/models/type_movement_model.py
@@ -0,0 +1,15 @@
+from db import db
+from sqlalchemy import Integer, String, Text, Boolean, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+
+class TypeMovement(db.Model):
+
+    __tablename__ = "type_movements"
+
+    id_type_movement: Mapped[id] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    status: Mapped[bool] = mapped_column(Boolean, default=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
