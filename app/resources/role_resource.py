0f957c1bdfafb6c2b802a4115b7ff8b8a5d9103a
leandro291 <lenadrito1@gmail.com>
2026-06-29 17:34:48 -0500

    Agregar manejo de errores y respuestas en los métodos get, put y delete de RoleResource y ManageRolResource

--- app/resources/role_resource.py
+++ app/resources/role_resource.py
@@ -7,6 +7,19 @@ from app.schemas.role_schema import RoleSchema
 
 class RoleResource(Resource):
     
+    def get(self):
+        
+        try:
+
+            roles: list[Role] = Role.query.all()
+            roles_list = [role.to_json() for role in roles]
+            return roles_list
+
+        except Exception as e:
+            return {
+                'error': str(e)
+            }, 400
+        
     def post(self):
         try:
 
@@ -39,15 +52,78 @@ class RoleResource(Resource):
                 'error': str(e)
             }, 400
 
-    def get(self):
-        
-        try:
 
-            roles: list[Role] = Role.query.all()
-            roles_list = [role.to_json() for role in roles]
-            return roles_list
+class ManageRolResource(Resource):
+
+    def get(self, id_role: int):
+        try:
+            
+            rol: Role = Role.query.filter_by(id_role=id_role).first()
+
+            if rol is None:
+                return {
+                    'error': "rol nout found"
+                }, 400
+            
+            return rol.to_json(), 200
 
         except Exception as e:
             return {
                 'error': str(e)
-            }, 400
+            }
+
+    def put(self, id_role: int):
+        try:
+
+            rol: Role = Role.query.filter_by(id_role=id_role).first()
+
+            if rol is None:
+                return {
+                    'error': "rol nout found"
+                }, 400
+
+            data = request.get_json()
+            validated_data = RoleSchema.model_validate(data)
+
+            name = Role.query.filter_by(name=validated_data.name).first()
+
+            if name:
+                return {
+                    'error': 'role alredy exists'
+                }, 400
+            
+            rol.name = validated_data.name
+            rol.description = validated_data.description
+
+            db.session.commit()
+
+            return rol.to_json(), 200
+        
+        except ValidationError as e:
+            return {
+                'error': e.errors()
+            }
+        except Exception as e:
+            return {
+                'error': str(e)
+            }
+
+    def delete(self, id_role: int):
+        try:
+            rol: Role = Role.query.filter_by(id_role=id_role).first()
+
+            if rol is None:
+                return {
+                    'error': "rol nout found"
+                }, 400
+            
+            rol.status = False
+
+            db.session.commit()
+
+            return None, 200
+            
+        except Exception as e:
+            return {
+                'error': str(e)
+            }
