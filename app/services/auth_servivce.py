f1529270531643f0476ba1a00da035fbe14274ff
leandro291 <lenadrito1@gmail.com>
2026-06-30 23:38:38 -0500

    Agregar servicio AuthService para manejar autenticación de usuarios

---
+++ app/services/auth_servivce.py
@@ -0,0 +1,47 @@
+from app.models.user_model import User
+from app.utils.helpers import password_helper
+from app.utils.helpers import crypto_helper
+from db import db
+from flask_jwt_extended import create_access_token, create_refresh_token
+
+class AuthService:
+
+    def get_by_email(self, email: str) -> User | None:
+        email = User.query.filter_by(email=email).first()
+        return email
+    
+    def register(self, data: User) -> User:
+
+        create_user = User(
+            name=data.name,
+            last_name=data.last_name,
+            email=data.email,
+            password=password_helper.hash_password(data.password)
+        )
+
+        db.session.add(create_user)
+        db.session.commit()
+        
+        return create_user
+    
+    def login(self, user: User) -> User:
+
+        hashed_id = crypto_helper.encrypt(user.id_user)
+
+        access_token = create_access_token(
+            identity=hashed_id,
+            additional_claims={
+                'name': user.name,
+                'last_name': user.last_name,
+                'email': user.email
+            }
+        )
+
+        refresh_token = create_refresh_token(
+            identity=hashed_id
+        )
+
+        return access_token, refresh_token
+    
+    
+auth_service = AuthService()
