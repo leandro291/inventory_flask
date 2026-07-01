01fcd1918ac4022a9fe447e43f375471909c5adc
leandro291 <lenadrito1@gmail.com>
2026-06-30 23:44:08 -0500

    Agregar configuración de expiración para tokens JWT en la clase Config

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
 
 load_dotenv()
 
@@ -7,3 +8,5 @@ class Config:
 
     DEBUG = True
     SQLALCHEMY_DATABASE_URI=os.getenv('DB_URI')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
