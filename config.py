c6f6480f23e32aff566cde1d8cd46a20063439c2
leandro291 <lenadrito1@gmail.com>
2026-07-01 19:35:39 -0500

    Agregar SECRET_KEY y JWT_SECRET_KEY a la clase Config

--- config.py
+++ config.py
@@ -8,5 +8,8 @@ class Config:
 
     DEBUG = True
     SQLALCHEMY_DATABASE_URI=os.getenv('DB_URI')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    
+    JWT_SECRET_KEY=os.getenv('JWT_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
