7b9fc626acd4e1a3d7bdafc0230353e97bb087ad
leandro291 <lenadrito1@gmail.com>
2026-07-01 22:53:48 -0500

    Agregar recursos SupplierResource y ManagerSupplierResource para manejar operaciones CRUD de proveedores

--- app/router.py
+++ app/router.py
@@ -5,6 +5,7 @@ from app.resources.category_resource import CategoryResource, ManagerCategoryRes
 from app.resources.company_resource import CompanyResource, ManagerCompanyResource
 from app.resources.role_resource import RoleResource, ManagerRoleResource
 from app.resources.auth_resource import LoginResource, RegisterResource
+from app.resources.supplier_resource import SupplierResource, ManagerSupplierResource
 
 api = Api(app, prefix="/api/v1")
 
@@ -16,6 +17,9 @@ api.add_resource(ManagerCategoryResource, "/category/<int:id_category>")
 
 api.add_resource(RoleResource, "/role")
 api.add_resource(ManagerRoleResource, "/role/<int:id_role>")
+
+api.add_resource(SupplierResource, "/supplier")
+api.add_resource(ManagerSupplierResource, "/supplier/<int:id_supplier>")
 
 api.add_resource(CompanyResource, "/company")
 api.add_resource(ManagerCompanyResource, "/company/<int:id_company>")
