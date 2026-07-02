d938760afb5b911abddb949a9f27d30a15833a52
leandro291 <lenadrito1@gmail.com>
2026-07-01 20:47:37 -0500

    Agregar recurso CompanyResource y ManagerCompanyResource para manejar operaciones CRUD de empresas

---
+++ app/resources/company_resource.py
@@ -0,0 +1,107 @@
+from flask_restful import Resource
+from app.services.company_service import company_service
+from app.models.company_model import Company
+from app.schemas.company_schema import CompanySchema
+from pydantic import ValidationError
+from flask import request
+
+class CompanyResource(Resource):
+    
+    def get(self):
+        try:
+
+            companies: list[Company] = company_service.get_all()
+            companies_list = [company.to_json() for company in companies]
+            return companies_list, 200
+
+        except Exception as e:
+            return {
+                'error': str(e)
+            }, 400
+
+    def post(self):
+        try:
+            data = request.get_json()
+            validated_data = CompanySchema.model_validate(data)
+
+            company = company_service.get_by_name(validated_data.name)
+
+            if company:
+                return {
+                    'error': 'company name already exists'
+                }, 400
+            
+            create_company = company_service.create(validated_data)
+
+            return create_company.to_json(), 200
+            
+        except ValidationError as e:
+            return {
+                'error': e.errors()
+            }
+        except Exception as e:
+            return {
+                'error': str(e)
+            }, 400
+
+class ManagerCompanyResource(Resource):
+    
+    def get(self, id_company: int):
+        try:
+            
+            company = company_service.get_by_id(id_company)
+
+            if company is None:
+                return {
+                    'error': 'company not found'
+                }, 404
+            
+            return company.to_json(), 200
+
+        except Exception as e:
+            return {
+                'error': str(e)
+            }, 400
+
+    def put(self, id_company):
+        try:
+            company = company_service.get_by_id(id_company)
+
+            if company is None:
+                return {
+                    'error': 'company not found'
+                }, 404
+            
+            data = request.get_json()
+            validated_data = CompanySchema.model_validate(data)
+
+            company_update = company_service.update(company, validated_data)
+
+            return company_update.to_json(), 200
+            
+        except ValidationError as e:
+            return {
+                'error': e.errors()
+            }
+        except Exception as e:
+            return {
+                'error': str(e)
+            }, 400
+
+    def delete(self, id_company):
+        try:
+            company = company_service.get_by_id(id_company)
+
+            if company is None:
+                return {
+                    'error': 'company not found'
+                }, 404
+            
+            company_delete = company_service.delete(company)
+
+            return company_delete.to_json(), 200
+            
+        except Exception as e:
+            return {
+                'error': str(e)
+            }, 400
