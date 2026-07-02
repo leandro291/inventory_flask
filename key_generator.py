26f8863cb1433c8dea319928b65d1a223013bd9d
leandro291 <lenadrito1@gmail.com>
2026-07-01 19:36:06 -0500

    Agregar generación de claves SECRET_KEY y JWT_SECRET_KEY en key_generator.py

--- key_generator.py
+++ key_generator.py
@@ -1,4 +1,10 @@
 from cryptography.fernet import Fernet
+import secrets
 
 key = Fernet.generate_key()
-print(key.decode())
+llave_flask = secrets.token_hex(32)
+llave_jwt = secrets.token_hex(32)
+
+print(f"FERNET_SECRET_KEY: {key.decode()}")
+print(f"SECRET_KEY= {llave_flask}")
+print(f"JWT_SECRET_KEY= {llave_jwt}")
