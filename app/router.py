7dabdad933263413dbc31f28b5026614b56067a1
leandro291 <lenadrito1@gmail.com>
2026-07-01 20:47:27 -0500

    Reorganizar recursos en router.py para mejorar la estructura del API

--- app/router.py
+++ app/router.py
@@ -1,12 +1,16 @@
 from app import app
 from flask_restful import Api
+from app.resources.company_resource import CompanyResource, ManagerCompanyResource
 from app.resources.role_resource import RoleResource, ManageRolResource
 from app.resources.auth_resource import LoginResource, RegisterResource
 
 api = Api(app, prefix="/api/v1")
 
+api.add_resource(LoginResource, "/login")
+api.add_resource(RegisterResource, "/register")
+
 api.add_resource(RoleResource, "/role")
 api.add_resource(ManageRolResource, "/role/<int:id_role>")
 
-api.add_resource(LoginResource, "/login")
-api.add_resource(RegisterResource, "/register")
+api.add_resource(CompanyResource, "/company")
+api.add_resource(ManagerCompanyResource, "/company/<int:id_company>")
