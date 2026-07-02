919c27f1c6a18821ef7262c28afd2e7bd4785792
leandro291 <lenadrito1@gmail.com>
2026-07-01 19:36:20 -0500

    Agregar recursos de autenticación para inicio de sesión y registro en router.py

--- app/router.py
+++ app/router.py
@@ -1,8 +1,12 @@
 from app import app
 from flask_restful import Api
 from app.resources.role_resource import RoleResource, ManageRolResource
+from app.resources.auth_resource import LoginResource, RegisterResource
 
 api = Api(app, prefix="/api/v1")
 
 api.add_resource(RoleResource, "/role")
 api.add_resource(ManageRolResource, "/role/<int:id_role>")
+
+api.add_resource(LoginResource, "/login")
+api.add_resource(RegisterResource, "/register")
