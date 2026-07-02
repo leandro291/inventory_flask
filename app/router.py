a55fb7c90138ceb15d30617b1a6bcc13069a6187
leandro291 <lenadrito1@gmail.com>
2026-07-01 21:48:21 -0500

    Actualizar rutas de autenticación y recursos de categoría en router.py

--- app/router.py
+++ app/router.py
@@ -1,16 +1,21 @@
 from app import app
 from flask_restful import Api
+
+from app.resources.category_resource import CategoryResource, ManagerCategoryResource
 from app.resources.company_resource import CompanyResource, ManagerCompanyResource
-from app.resources.role_resource import RoleResource, ManageRolResource
+from app.resources.role_resource import RoleResource, ManagerRoleResource
 from app.resources.auth_resource import LoginResource, RegisterResource
 
 api = Api(app, prefix="/api/v1")
 
-api.add_resource(LoginResource, "/login")
-api.add_resource(RegisterResource, "/register")
+api.add_resource(LoginResource, "/auth/login")
+api.add_resource(RegisterResource, "/auth/register")
+
+api.add_resource(CategoryResource, "/category")
+api.add_resource(ManagerCategoryResource, "/category/<int:id_category>")
 
 api.add_resource(RoleResource, "/role")
-api.add_resource(ManageRolResource, "/role/<int:id_role>")
+api.add_resource(ManagerRoleResource, "/role/<int:id_role>")
 
 api.add_resource(CompanyResource, "/company")
 api.add_resource(ManagerCompanyResource, "/company/<int:id_company>")
