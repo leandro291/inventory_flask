b03973d34d0d77b9a822eb269a30a42e7dc9249d
leandro291 <lenadrito1@gmail.com>
2026-07-01 19:36:14 -0500

    Agregar inicialización de JWTManager en __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from db import db
 from flask import Flask
 from config import Config
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
 
 from app.models import (
     role_model,
