e14486c0512f67eea12cc25abce09bf1d00a81c5
leandro291 <lenadrito1@gmail.com>
2026-07-04 11:57:00 -0500

    Refactor ProductService: reorganizar métodos y mejorar la legibilidad del código

--- app/services/product_service.py
+++ app/services/product_service.py
@@ -8,18 +8,17 @@ class ProductService:
         products = Product.query.filter_by(status=True).all()
         return products
     
+    def get_by_id(self, id_product: int) -> Product | None:
+        product = Product.query.filter_by(id_product=id_product).first()
+        return product
+    
     def get_last(self) -> Product | None:
         product = Product.query.order_by(
             Product.id_product.desc()
         ).first()
         return product
 
-    def create(
-            self,
-            data: ProductSchema,
-            code: str,
-            image: str
-    ) -> Product:
+    def create(self, data: ProductSchema, code: str, image: str) -> Product:
         product = Product(
             name=data.name,
             code=code,
@@ -30,9 +29,32 @@ class ProductService:
             sale_price=data.sales_price,
             id_category=data.id_category
         )
+
         db.session.add(product)
         db.session.commit()
 
         return product
     
+    def update(self, data: ProductSchema, product: Product, image: str | None) -> Product:
+
+        if image:
+            product.image = image
+
+        product.name = data.name
+        product.description = data.description
+        product.brand = data.brand
+        product.purchase_price = data.purchase_price
+        product.sale_price = data.sales_price
+        product.id_category = data.id_category
+
+        db.session.commit()
+
+        return product
+    
+    def delete(self, product: Product) -> None:
+
+        product.status = False
+
+        db.session.commit()
+
 product_service = ProductService()
