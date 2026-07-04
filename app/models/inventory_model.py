379f278792b3e2108b76f0c955b6d7c51cdf1baf
leandro291 <lenadrito1@gmail.com>
2026-07-04 12:25:50 -0500

    Agregar método to_json en el modelo Inventory para facilitar la serialización a JSON

--- app/models/inventory_model.py
+++ app/models/inventory_model.py
@@ -14,3 +14,14 @@ class Inventory(db.Model):
     updated_at: Mapped[datetime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
     id_product: Mapped[int] = mapped_column(ForeignKey('products.id_product'), nullable=False)
     id_repository: Mapped[int] = mapped_column(ForeignKey('repositories.id_repository'), nullable=False)
+
+    def to_json(self):
+        return {
+            'id_inventory': self.id_inventory,
+            'stock': self.stock,
+            'id_product': self.id_product,
+            'id_repository': self.id_repository,
+            'is_active': self.is_active,
+            'created_at': str(self.created_at),
+            'updated_at': str(self.updated_at),
+        }
