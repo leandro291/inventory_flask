4b5b0ba38bd222f9e84573a4a21a97ac57680b81
leandro291 <lenadrito1@gmail.com>
2026-07-04 12:26:23 -0500

    Agregar servicio InventoryService con métodos para gestionar inventarios

---
+++ app/services/inventory_service.py
@@ -0,0 +1,44 @@
+from app.models.inventory_model import Inventory
+from app.schemas.inventory_schema import InventorySchema
+from db import db
+
+
+class InventoryService:
+
+    def get_all(self) -> list[Inventory]:
+        inventories = Inventory.query.filter_by(is_active=True).all()
+        return inventories
+
+    def get_by_id(self, id_inventory: int) -> Inventory | None:
+        inventory = Inventory.query.filter_by(id_inventory=id_inventory).first()
+        return inventory
+
+    def get_by_product_and_repository(self, id_product: int, id_repository: int) -> Inventory | None:
+        inventory = Inventory.query.filter_by(
+            id_product=id_product,
+            id_repository=id_repository,
+        ).first()
+        return inventory
+
+    def create(self, data: InventorySchema) -> Inventory:
+        inventory = Inventory(
+            stock=data.stock,
+            id_product=data.id_product,
+            id_repository=data.id_repository,
+        )
+        db.session.add(inventory)
+        db.session.commit()
+        return inventory
+
+    def update(self, inventory: Inventory, data: InventorySchema) -> Inventory:
+        inventory.stock = data.stock
+        db.session.commit()
+        return inventory
+
+    def delete(self, inventory: Inventory) -> Inventory:
+        inventory.is_active = False
+        db.session.commit()
+        return inventory
+
+
+inventory_service = InventoryService()
