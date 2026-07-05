b8000bf07fbe7a3da5be8202219a47bcbb9dc891
leandro291 <lenadrito1@gmail.com>
2026-07-05 13:08:19 -0500

    Agregar datos de estadísticas, carga y error al componente Header en HomePage

--- frontend/src/features/pages/home/home_page.jsx
+++ frontend/src/features/pages/home/home_page.jsx
@@ -1,9 +1,12 @@
+import { useHome } from './hooks/useHome.js'
 import { Header } from './components/Header/header.jsx'
 
 export function HomePage() {
+  const { stats, loading, error } = useHome()
+
   return (
     <main>
-      <Header />
+      <Header stats={stats} loading={loading} error={error} />
     </main>
   )
 }
