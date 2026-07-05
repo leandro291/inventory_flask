0b29417572a4be98796175a6e06517fa9102abed
leandro291 <lenadrito1@gmail.com>
2026-07-05 11:31:41 -0500

    Agregar servicio y utilidades para la gestión de movimientos

---
+++ frontend/src/features/pages/movements/utils/movementFormUtils.js
@@ -0,0 +1,9 @@
+export const EMPTY_FORM = {
+  observation: '',
+  id_supplier: '',
+  id_type_movement: '',
+  id_repository: '',
+  details: [],
+}
+
+export const EMPTY_DETAIL = { id_product: '', quantity: '', unit_price: '' }
