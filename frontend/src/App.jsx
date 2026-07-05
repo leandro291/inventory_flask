33578846097dcc10fd087714e44f2dae5cd44bef
leandro291 <lenadrito1@gmail.com>
2026-07-05 13:04:32 -0500

    Agregar inicialización del estado de autenticación en el componente App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react'
 import { RouterProvider } from 'react-router'
 import { router } from './router/index.jsx'
+import { useAuthStore } from './features/pages/auth/store/auth-store.js'
 
 function App() {
+  const init = useAuthStore((s) => s.init)
+
+  useEffect(() => { init() }, [init])
+
   return <RouterProvider router={router} />
 }
 
