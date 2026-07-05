3e361059ac30773d16d633df794b82d108175e41
leandro291 <lenadrito1@gmail.com>
2026-07-05 11:13:44 -0500

    Agregar componentes y lógica para la gestión de categorías en la página de categorías

---
+++ frontend/src/app/categories/categories.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { CategoriesPage } from '../../features/pages/categories/pages/categories_page'
+
+export function Categories() {
+    return (
+        <CategoriesPage/>
+    )
+}
