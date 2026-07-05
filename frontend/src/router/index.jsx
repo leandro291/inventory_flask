7552ca7beb755adbdb8de547600e5e367899b317
leandro291 <lenadrito1@gmail.com>
2026-07-05 11:32:29 -0500

    Agregar rutas para la gestión de categorías, inventarios, compañías, repositorios, roles, proveedores y movimientos

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -2,7 +2,13 @@ import { createBrowserRouter, Navigate } from 'react-router'
 import { MainPage } from '../common/layouts/main_page.jsx'
 import { Home } from '../app/home/home.jsx'
 import { Products } from '../app/products/products.jsx'
-
+import { Categories } from '../app/categories/categories.jsx'
+import { Inventory } from '../app/inventory/inventory.jsx'
+import { Companies } from '../app/companies/companies.jsx'
+import { Repositories } from '../app/repositories/repositories.jsx'
+import { Roles } from '../app/roles/roles.jsx'
+import { Suppliers } from '../app/suppliers/suppliers.jsx'
+import { Movements } from '../app/movements/movements.jsx'
 export const router = createBrowserRouter([
   {
     path: '/',
@@ -19,6 +25,34 @@ export const router = createBrowserRouter([
       {
         path: 'products',
         element: <Products />,
+      },
+      {
+        path: 'categories',
+        element: <Categories />,
+      },
+      {
+        path: 'inventory',
+        element: <Inventory />,
+      },
+      {
+        path: 'companies',
+        element: <Companies />,
+      },
+      {
+        path: 'repositories',
+        element: <Repositories />,
+      },
+      {
+        path: 'roles',
+        element: <Roles />,
+      },
+      {
+        path: 'suppliers',
+        element: <Suppliers />,
+      },
+      {
+        path: 'movements',
+        element: <Movements />,
       },
     ],
   },
